1f4238ca820c4556ff4fdda9d805df6a51feddf8
graycat27 <64903817+graycat27@users.noreply.github.com>
2022-03-04 22:31:04 +0900

    add usage

--- src/js/referenceDocs.js
+++ src/js/referenceDocs.js
@@ -3,9 +3,28 @@
  * https://yui-kitamura.eng.pro
  *
  * referenceDocs-js
- * version 0.1 released on 2022.03.03.
+ * version 0.1 released on 2022.03.04.
  * This provide reference documents list for your HTML document.
  *
+ * usage
+ * <p>it says "quoted document"<ref-doc doc-id="uniqueId"></ref-doc>.
+ * Also the other one says "document quoted"<ref-doc doc-id="webSource"></ref-doc>.</p>
+ * <ref-doc>
+ *    <ref-doc-id>uniqueId</ref-doc-id>
+ *    <ref-doc-name>document name</ref-doc-name>
+ *    <ref-doc-author>who write it</ref-doc-author>
+ *    <ref-book-publisher>published by</ref-book-publisher>
+ *    <ref-book-published-date>2022/03/02</ref-book-published-date>
+ *    <ref-book-pages>p.27</ref-book-pages>
+ * </ref-doc>
+ * <ref-doc>
+ *    <ref-doc-id>webSource</ref-doc-id>
+ *    <ref-doc-name>web page name</ref-doc-name>
+ *    <ref-doc-author>who write it</ref-doc-author>
+ *    <ref-web-link>https://yui-kitamura.eng.pro/hoge</ref-web-link>
+ *    <ref-web-visited-date>2022/03/04</ref-web-visited-date>
+  * </ref-doc>
+ * <ref-doc-list></ref-doc-list>
  */
 let referenceDocs = (function () {
     'use strict';
