695de62fcb90a5c10a10012d732fd4da5607f104
graycat27 <64903817+graycat27@users.noreply.github.com>
2022-03-04 21:17:11 +0900

    first commit js file

---
+++ src/js/referenceDocs.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) yui-Kitamura 2022
+ * https://yui-kitamura.eng.pro
+ *
+ * referenceDocs-js
+ * version 0.1 released on 2022.03.03.
+ * This provide reference documents list for your HTML document.
+ *
+ */
+let referenceDocs = (function () {
+    'use strict';
+
+    let fn = {
+
+    };
+
+    return {
+    	fn: fn
+    };
+}());
